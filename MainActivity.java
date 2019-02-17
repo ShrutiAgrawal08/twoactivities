d07f4b116ccb9ec6b4cea4a3c167a57f8fe106e2
ShrutiAgrawal08 <shruti.agrawal_mca18@gla.ac.in>
2019-02-17 09:24:52 -0800

    add logcat e

--- MainActivity.java
+++ MainActivity.java
@@ -2,12 +2,52 @@ package agrawal.shruti.twoactivities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(MainActivity.this, "onStart", Toast.LENGTH_SHORT).show();
+        Log.e("MainActivity:onStart", "called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(MainActivity.this, "onResume", Toast.LENGTH_SHORT).show();
+        Log.e(TAG, "onResume: called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+        Log.e(TAG, "onPause: called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+        Log.e(TAG, "onStop: called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+        Log.e(TAG, "onDestroy: called");
+    }
 }
+
+
